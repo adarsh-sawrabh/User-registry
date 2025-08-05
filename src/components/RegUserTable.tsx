352e5fea670cebf6983d90b04ee50828db532938
Adarsh Kumar <adarshkumar6@kpmg.com>
2025-08-05 22:25:18 +0530

    User details table

--- src/components/RegUserTable.tsx
+++ src/components/RegUserTable.tsx
@@ -12,7 +12,7 @@ import {
     Paper,
     Tooltip,
     Box,
-    Card,
+    Grid,
 } from '@mui/material';
 import { Edit, Delete } from '@mui/icons-material';
 
@@ -91,79 +91,92 @@ const RegUserTable: React.FC = () => {
     };
 
     return (
-        <Box sx={{ padding: 3 }}>
-            <Typography variant="h3" sx={{ 
-                mb: 3, 
-                backgroundColor:'rgba(9, 89, 242, 0.78)', 
-                fontSize: '30px',
-                color: '#FFFFFF',
-                padding: 1
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ 
+                padding: { xs: 2, sm: 3, md: 4 }, 
+                marginTop: 2,
                 }}>
-                User Details
-            </Typography>
-            <TableContainer component={Card} sx={{ padding: 5 }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell padding="checkbox">
-                                <Checkbox
-                                    indeterminate={selected.length > 0 && selected.length < data.length}
-                                    checked={data.length > 0 && selected.length === data.length}
-                                    onChange={handleSelectAllClick}
-                                    aria-label="select all users"
-                                />
-                            </TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Email</TableCell>
-                            <TableCell>Phone Number</TableCell>
-                            <TableCell>Event</TableCell>
-                            <TableCell>Organization</TableCell>
-                            <TableCell>Address</TableCell>
-                            <TableCell>Areas of Interest</TableCell>
-                            <TableCell>Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map((user) => {
-                            const isItemSelected = isSelected(user.id);
-                            return (
-                                <TableRow
-                                    hover
-                                    key={user.id}
-                                    selected={isItemSelected}
-                                    onClick={() => handleClick(user.id)}
-                                >
-                                    <TableCell padding="checkbox">
-                                        <Checkbox
-                                            checked={isItemSelected}
-                                            aria-labelledby={`enhanced-table-checkbox-${user.id}`}
-                                        />
-                                    </TableCell>
-                                    <TableCell>{user.name}</TableCell>
-                                    <TableCell>{user.email}</TableCell>
-                                    <TableCell>{user.phoneNumber}</TableCell>
-                                    <TableCell>{user.event}</TableCell>
-                                    <TableCell>{user.organization}</TableCell>
-                                    <TableCell>{user.address}</TableCell>
-                                    <TableCell>{user.areasOfInterest}</TableCell>
-                                    <TableCell>
-                                        <Tooltip title="Edit">
-                                            <IconButton onClick={() => handleEdit(user.id)} aria-label="edit">
-                                                <Edit />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Tooltip title="Delete">
-                                            <IconButton onClick={() => handleDelete(user.id)} aria-label="delete">
-                                                <Delete />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                <Grid container justifyContent="center" spacing={2} sx={{backgroundColor: '#e7eff3ff', borderRadius: 1}}>
+                    <Box sx={{ backgroundColor: '#0073e6', padding: 1, borderRadius: 1, width: '100%' }}>
+                        <Typography variant="h5" sx={{ color: '#fff', textAlign: 'center', fontSize:'20px', p:1}}>
+                            User Details
+                        </Typography>
+                    </Box>
+                    <Box sx={{
+                        width: '96%',
+                        borderRight: "3px solid #cccccc3b",
+                        borderBottom: "3px solid #cccccc3b",
+                        borderLeft: "3px solid #cccccc3b",
+                        boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 8px 0px",
+                    }}>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell padding="checkbox">
+                                            <Checkbox
+                                                indeterminate={selected.length > 0 && selected.length < data.length}
+                                                checked={data.length > 0 && selected.length === data.length}
+                                                onChange={handleSelectAllClick}
+                                                aria-label="select all users"
+                                            />
+                                        </TableCell>
+                                        <TableCell sx={{ width: '12%' }}>Name</TableCell>
+                                        <TableCell sx={{ width: '18%' }}>Email</TableCell>
+                                        <TableCell sx={{ width: '15%' }}>Phone Number</TableCell>
+                                        <TableCell sx={{ width: '10%' }}>Event</TableCell>
+                                        <TableCell sx={{ width: '13%' }}>Organization</TableCell>
+                                        <TableCell sx={{ width: '15%' }}>Address</TableCell>
+                                        <TableCell sx={{ width: '10%' }}>Areas of Interest</TableCell>
+                                        <TableCell sx={{ width: '15%' }}>Actions</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {data.map((user) => {
+                                        const isItemSelected = isSelected(user.id);
+                                        return (
+                                            <TableRow
+                                                hover
+                                                key={user.id}
+                                                selected={isItemSelected}
+                                                onClick={() => handleClick(user.id)}
+                                            >
+                                                <TableCell padding="checkbox">
+                                                    <Checkbox
+                                                        checked={isItemSelected}
+                                                        aria-labelledby={`enhanced-table-checkbox-${user.id}`}
+                                                    />
+                                                </TableCell>
+                                                <TableCell>{user.name}</TableCell>
+                                                <TableCell>{user.email}</TableCell>
+                                                <TableCell>{user.phoneNumber}</TableCell>
+                                                <TableCell>{user.event}</TableCell>
+                                                <TableCell>{user.organization ? user.organization : 'NA'}</TableCell>
+                                                <TableCell>{user.address ? user.address : 'NA'}</TableCell>
+                                                <TableCell>{user.areasOfInterest ? user.areasOfInterest : 'NA'}</TableCell>
+                                                <TableCell>
+                                                    <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+                                                        <Tooltip title="Edit">
+                                                            <IconButton onClick={() => handleEdit(user.id)} aria-label="edit" color="primary">
+                                                                <Edit />
+                                                            </IconButton>
+                                                        </Tooltip>
+                                                        <Tooltip title="Delete">
+                                                            <IconButton onClick={() => handleDelete(user.id)} aria-label="delete" color="secondary">
+                                                                <Delete />
+                                                            </IconButton>
+                                                        </Tooltip>
+                                                    </Box>
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Box>
+                </Grid>
+            </Box>
         </Box>
     )
 }
